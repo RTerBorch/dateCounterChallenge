9f79d815dfba7a75f94a88e9b6639278d5692b3c
RTerBorch <robin.ter.borch@gmail.com>
2024-01-28 10:30:41 +0100

    added steps and multiplier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [multiplier, setMultiplier] = useState(0);
+  const [days, setDays] = useState(0);
+
+  useEffect(() => {
+    setDays(step * multiplier);
+  }, [step, multiplier]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
+
+  function addStep() {
+    setStep(step + 1);
+  }
+  function removeStep() {
+    setStep(step - 1);
+  }
+  function addMultiplier() {
+    setMultiplier(multiplier + 1);
+  }
+  function removeMultiplier() {
+    setMultiplier(multiplier - 1);
+  }
+
+  function today() {
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + days);
+    const options = { year: "numeric", month: "numeric", day: "numeric" };
+    return currentDate.toLocaleDateString(undefined, options);
+  }
+
+  function Counter() {
+    return (
+      <div>
+        <div>
+          <button onClick={removeStep}>-</button>
+          {`Step: ${step}`}
+          <button onClick={addStep}>+</button>
+          <button onClick={removeMultiplier}>-</button>
+          {`multiplier: ${multiplier}`}
+          <button onClick={addMultiplier}>+</button>
+        </div>
+        <p>{`${days} days from today: ${today()}`}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
