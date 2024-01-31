bc0331f428b4db68fc027eccb57a3b6790f1e667
RTerBorch <robin.ter.borch@gmail.com>
2024-01-31 13:22:43 +0100

    added slider and reset button

--- src/App.js
+++ src/App.js
@@ -1,53 +1,69 @@
+import { useState } from "react";
 import "./App.css";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [step, setStep] = useState(1);
-  const [multiplier, setMultiplier] = useState(0);
-  const [days, setDays] = useState(0);
-
-  useEffect(() => {
-    setDays(step * multiplier);
-  }, [step, multiplier]);
-
   return (
     <div className="App">
-      <p>---------- What day? ----------</p>
       <Counter />
-      <p>-------------------------------</p>
     </div>
   );
+}
 
-  function today() {
-    const currentDate = new Date();
-    currentDate.setDate(currentDate.getDate() + days);
-    const options = {
-      weekday: "long",
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-    };
-    return currentDate.toLocaleDateString(undefined, options);
+function Counter() {
+  const [multiplier, setMultiplier] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function handleReset() {
+    setMultiplier(0);
+    setStep(1);
   }
 
-  function Counter() {
-    return (
-      <>
-        <div>
-          <button onClick={() => setStep((s) => s - 1)}>-</button>
-          <span> {`Step: ${step}`}</span>
-          <button onClick={() => setStep((s) => s + 1)}>+</button>
+  const calculateDate = () => {
+    const baseDate = new Date();
+    baseDate.setDate(baseDate.getDate() + multiplier);
+    return baseDate.toDateString();
+  };
 
-          <button onClick={() => setMultiplier((m) => m - 1)}>-</button>
-          <span>{`multiplier: ${multiplier}`}</span>
-          <button onClick={() => setMultiplier((m) => m + 1)}>+</button>
-        </div>
-        <p>
-          {days === 0 ? "Today is: " : `${days} days from today: `} {today()}
-        </p>
-      </>
-    );
-  }
+  return (
+    <>
+      <p>---------- What day? ----------</p>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>Step: {step}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setMultiplier((m) => m - step)}>-</button>
+        <input
+          type="text"
+          value={multiplier}
+          onChange={(e) => setMultiplier(Number(e.target.value))}
+        />
+        <button onClick={() => setMultiplier((m) => m + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {multiplier === 0
+            ? "Today is "
+            : multiplier > 0
+            ? `${multiplier} days from today is `
+            : `${Math.abs(multiplier)} days ago was `}
+        </span>
+        <span>{calculateDate()}</span>
+      </p>
+
+      {(multiplier !== 0 || step !== 1) && (
+        <button onClick={handleReset}>Reset</button>
+      )}
+    </>
+  );
 }
 
 export default App;
